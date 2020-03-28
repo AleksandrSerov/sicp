33ef0c4fa6532f76fffe484648fee10e6a596f87
Aleksandr Serov <sashaserov2010@gmail.com>
2020-03-28 23:43:53 +0300

    ex 1 9

---
+++ sicp/chapter1/ex1_9.rkt
@@ -0,0 +1,25 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (+ a b)
+    (if (= a 0)
+        b
+        (inc (+ (dec a) b))))
+; (+ 3 4)
+; (inc (+ 2 4))
+; (inc (inc (+ 1 4)))
+; (inc (inc (inc (+ 0 4))))
+; (inc (inc (inc (4))))
+; 7
+; Рекурсивный процесс
+(define (+ a b)
+    (if (= a 0)
+        b
+        (+ (dec a) (inc b))))
+; (+ 3 4)
+; (+ 2 5)
+; (+ 1 6)
+; (+ 0 7)
+; 7
+; Итеративный процесс
