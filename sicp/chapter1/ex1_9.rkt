16c08185fb016aceb9f8328212071581eceab93d
Aleksandr Serov <sashaserov2010@gmail.com>
2020-03-29 18:51:34 +0300

    done 1_10

--- sicp/chapter1/ex1_9.rkt
+++ sicp/chapter1/ex1_9.rkt
@@ -2,10 +2,10 @@
 
 (#%require rackunit)
 
-(define (+ a b)
-    (if (= a 0)
-        b
-        (inc (+ (dec a) b))))
+; (define (+ a b)
+;     (if (= a 0)
+;         b
+;         (inc (+ (dec a) b))))
 ; (+ 3 4)
 ; (inc (+ 2 4))
 ; (inc (inc (+ 1 4)))
@@ -13,10 +13,10 @@
 ; (inc (inc (inc (4))))
 ; 7
 ; Рекурсивный процесс
-(define (+ a b)
-    (if (= a 0)
-        b
-        (+ (dec a) (inc b))))
+; (define (+ a b)
+;     (if (= a 0)
+;         b
+;         (+ (dec a) (inc b))))
 ; (+ 3 4)
 ; (+ 2 5)
 ; (+ 1 6)
